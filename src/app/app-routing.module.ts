b53e605ddc5726af06283bfd623904d84ff80a5d
keke0123 <keke0123@naver.com>
2019-04-20 16:18:23 +0900

    sqlite sql file import 해서 사용하는 방식 성공

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
+  { path: 'developer/:id', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+import { DatabaseService, Dev } from './../services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  developers: Dev[] = [];
+  // products: Observable<any[]>;
+  constructor(private db: DatabaseService) {
+  }
+  ngOnInit() {
+    this.db.getDatabaseState().subscribe(rdy => {
+      if (rdy) {
+        this.db.getDevs().subscribe(devs => {
+          this.developers = devs;
+        });
+        // this.products = this.db.getProducts();
+      }
+    });
+  }
+  testBtn1() {
+    this.db.getTestData01()
+        .then(res => {
+          console.log(res);
+          console.log(res.rows.item(0).name);
+          console.log(res.rows.item(1).name);
+        })
+  }
 }
